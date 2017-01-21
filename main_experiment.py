b3ae81aa287264ee253d8c911de39cb86d9f31a3
Buckler89 <droghini.diego@gmail.com>
2017-01-21 11:00:43 +0100

    nulla di che

--- main_experiment.py
+++ main_experiment.py
@@ -16,12 +16,14 @@ experiment_mnist=autoencoder_diego.autoencoder_fall_detection(kernel_shape,numbe
                                                                 
 X_train, y_train, x_test, y_test = experiment_mnist.pre_process_data_mnist();
                                                                              
-#config, weight = experiment_mnist.network_architecture_autoencoder(X_train, y_train, x_test, y_test);
+config, weight = experiment_mnist.network_architecture_autoencoder(X_train, y_train, x_test, y_test);
 
-#experiment_mnist.reconstruct_images(x_test, config, weight);               
+experiment_mnist.reconstruct_images(x_test, config, weight);               
 
 image = img.imread("stupid_test_image/emoticon-5700-src-7edfa11b86dc141a-28x28.png")
 imageBN=image[:,:,0]
 imageBN2=np.zeros((1,1,28,28))
 imageBN2[0,0,:,:]=imageBN;
 experiment_mnist.reconstruct_image(imageBN2, config, weight);               
+
+                                  
