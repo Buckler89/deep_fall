0fdfd141a16acaa61e4623509964369b28b0be13
Unknown <ferretti.daniele710@gmail.com>
2017-02-15 14:49:32 +0100

    log_mel, delta and delta-deltas

--- spectrogram_extraction.py
+++ spectrogram_extraction.py
@@ -1,3 +1,4 @@
+#import matplotlib.pyplot as plt
 import numpy as np
 from scipy import signal
 import librosa
@@ -21,43 +22,48 @@ def spectrograms(source, dest, fs, N, overlap, win_type='hamming'):
         #Sxx = np.abs(X)**2
         Sxx = librosa.logamplitude(np.abs(X)**2,ref_power=np.max)
         np.save(path.join(dest,w[0:-4]),Sxx)
+
+def log_mel(source, dest, fs, N, overlap, win_type='hamming', n_mels=128, fmin=0.0, fmax=None, htk=True, delta_width=2):
+    # with htk=True use HTK formula instead of Slaney
+    # list all file in source directory
+    filenames = []
+    for (dirpath, dirnames,  filenames) in walk(source):
+        break
+    # drop all non wav file
+    wav_filenames = [f for f in filenames if f.lower().endswith('.wav')]
+
+    for w in wav_filenames:
+        coefficients = []
+        # Load an audio file as a floating point time series
+        x, fs  = librosa.core.load(path.join(source,w),sr=fs)
+        # Power spectrum
+        S = np.abs(librosa.core.stft(x, n_fft=N, window=signal.get_window(win_type,N), hop_length=N-overlap, center=False))**2
+        # Build a Mel filter
+        mel_basis = librosa.filters.mel(fs, N, n_mels, fmin, fmax, htk)
+        # Filtering
+        mel_filtered = np.dot(mel_basis, S)
         
-
-
-
-###############################################################################################    
-#    spec = []
-#    longest = 0
-#    for w in wav_filenames:
-#        ###################### calcola gli spettri
-#        if len(Sxx[1])>longest:
-#            longest=len(Sxx[1])
-#        spec.append([w, Sxx])
-#
-#
-## zeropadda gli spettrogrammi salvati in source e li salva in dest
-#def zeropads(source, dest):
-#    
-#
-#
-#
-#
-#
-#for w in spec:
-#    np.save(dest_path + w[0][0:-4],w[1],False)  
-#    w[1]=np.lib.pad(w[1], ((0, 0), (0, longest-len(w[1][1]))),'constant', constant_values=(0, 0))
-#    np.save(dest_path_zero_pad + w[0][0:-4],w[1],False)
-
-
-
+        coefficients.append(librosa.logamplitude(mel_filtered))
+        # add delta e delta-deltas
+        coefficients.append(librosa.feature.delta(mel_filtered, delta_width*2+1, order=1, axis=-1))
+        coefficients.append(librosa.feature.delta(mel_filtered, delta_width*2+1, order=2, axis=-1))
+        
+        np.save(path.join(dest,w[0:-4]),coefficients)
+    
 if __name__ == "__main__":
 
     wav_dir_path = '/home/daniele/Scrivania/all_file/'
     dest_path='/home/daniele/Scrivania/spectrograms/'
-    dest_path_zero_pad='//home/daniele/Scrivania/spectrograms_zero_pad/'
+    dest_path_log_mel='/home/daniele/Scrivania/logMels/'
     window_type = 'hamming'
     fft_length = 256
     overlap = 128
     Fs = 8000    
+    n_mels=13
+    fmin=0.0
+    fmax=Fs/2
+    htk=True
+    delta_width=2
     
     spectrograms(wav_dir_path, dest_path, Fs, fft_length, overlap, 'hamming')
+    log_mel(wav_dir_path, dest_path_log_mel, Fs, fft_length, overlap, window_type, n_mels, fmin, fmax, htk, delta_width)
