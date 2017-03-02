de6e870197da2866b1e823625aa299e5b8ac10d2
Buckler89 <droghini.diego@gmail.com>
2017-03-03 00:25:10 +0100

    Test now work. Logging is still too complicated, and print double when log in console

--- main_experiment.py
+++ main_experiment.py
@@ -235,11 +235,11 @@ model = net.model_fit(x_trains[0], _, nb_epoch=args.epoch, batch_size=args.batch
 
 for x_dev, y_dev in zip(x_devs, y_devs):  # sarebbero le fold
 
-    decoded_images = net.reconstruct_spectrogram(x_dev);
-    auc, optimal_th, _, _, _ = net.compute_score(x_dev, decoded_images, y_dev);
-    scoreAucNew[f] = auc;
+    decoded_images = net.reconstruct_spectrogram(x_dev)
+    auc, optimal_th, _, _, _ = net.compute_score(x_dev, decoded_images, y_dev)
+    scoreAucNew[f] = auc
     scoreThsNew[f] = optimal_th
-    f += 1;
+    f += 1
 
 logger.info("------------------------SCORE SELECTION---------------")
 
