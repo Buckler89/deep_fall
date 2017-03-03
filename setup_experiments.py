e8192baa540a67681d8e542603404e1bad8c9032
Unknown <ferretti.daniele710@gmail.com>
2017-03-03 11:36:39 +0100

    add setup_experiments

---
+++ setup_experiments.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 3 11:19:08 2017
+
+@author: daniele
+"""
+
+
+############################################ Parsing argomenti (intervalli, nomi file ecc...)
+
+
+############################################ definizione classe contenitore esperimento
+class experiment:
+    pass
+
+
+############################################ def grid_search:
+def grid_search():
+    pass
+
+############################################ def random_search:
+def random_search():
+    pass
+
+############################################ inizializzalizzazioni
+
+
+############################################ creazione della lista dei parametri secondo la strategia scelta
+
+
+############################################ creazione dei file per lo scheduler
