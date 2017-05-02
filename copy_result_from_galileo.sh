0963320291760c94b2c970044f75cca16d8e7802
Buckler89 <droghini.diego@gmail.com>
2017-05-02 12:05:27 +0200

    add script for parsing the TotalReport.csv file

--- copy_result_from_galileo.sh
+++ copy_result_from_galileo.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # -r for folder
-scp mfagiani@login.galileo.cineca.it:/gpfs/work/IscrC_DASHE/fall_detection/deep_fall/results/case5/totalReport.csv ./galielo_results/case5
+scp mfagiani@login.galileo.cineca.it:/gpfs/work/IscrC_DASHE/fall_detection/deep_fall/results/case2/totalReport.csv ./galielo_results/case2
